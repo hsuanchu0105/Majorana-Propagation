750615c598bcd17f32282c6378b6d0454ed757b4
Hsuanchu <hsuanchu@hsuanchu.localdomain>
2025-10-10 09:18:17 +0200

    Majorana prog

--- test/test_majorana.py
+++ test/test_majorana.py
@@ -1,2 +1,11 @@
 import unittest
 import numpy as np
+import cmath 
+
+class TestMajorana(unittest.TestCase):
+    def test_Majorana_Prop(self):
+        b0 = [1, 1]
+        M0 = MajoranaOp(2, b0)
+        theta1 = cmath.pi/3
+        b1 = [1, 0]
+        coeff1, coeff2, bnew = MajoranaPrg(M0, theta1, b1)
